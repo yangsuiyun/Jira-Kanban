fca08a25e6873ca1bebbf019c6a8bbd1bd003b08
Bidisha Das <officialbidisha1@gmail.com>
2021-12-12 00:50:04 +0530

    Left-nav css added properly.

--- src/app/layout/left-nav/left-nav.component.ts
+++ src/app/layout/left-nav/left-nav.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-left-nav',
   templateUrl: './left-nav.component.html',
-  styleUrls: ['./left-nav.component.scss']
 })
 export class LeftNavComponent implements OnInit {
   
