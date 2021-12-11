541378e505a12014161d7ab88308256f6fdad926
Bidisha Das <officialbidisha1@gmail.com>
2021-12-12 03:04:06 +0530

    Adding style for board.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { HttpClientModule } from '@angular/common/http';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,14 +8,26 @@ import { AppComponent } from './app.component';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { LeftNavComponent } from './layout/left-nav/left-nav.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { BoardComponent } from './pages/board/board.component';
 
+import {CardModule} from 'primeng/card';
+
+import {TableModule} from 'primeng/table';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 @NgModule({
   declarations: [
     AppComponent,
     LeftNavComponent,
+    HeaderComponent,
+    BoardComponent,
   ],
   imports: [
+    CardModule,
+    DragDropModule,
+    TableModule,
     BrowserModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     AppRoutingModule
   ],
---
+++ src/app/pages/board/board.component.ts
@@ -0,0 +1,45 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss'],
+})
+export class BoardComponent implements OnInit {
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  done = ['Take a shower', 'Check e-mail', 'Walk dog'];
+
+  xyz = ['A', 'B', 'C'];
+  constructor() { }
+  
+  ngOnInit() {
+    console.log(this.todo);
+    console.log(this.done);
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+
+    console.log(this.todo);
+    console.log(this.done);
+  }
+  
+}
+export enum Type {
+  TODO = 'To Do',
+  FUNCTIONAL_REVIEW = 'Functional Review',
+  DONE = 'Done',
+  IN_PROGRESS = 'In Progress',
+  QA = 'QA',
+  BLOCKED = 'BLOCKED'
+}
