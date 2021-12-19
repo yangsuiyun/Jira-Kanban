1798d9e00470b105f7e35e069dffe2f198ab633b
Bidisha Das <officialbidisha1@gmail.com>
2021-12-20 03:14:48 +0530

    Added appropriate routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { BoardComponent } from './pages/board/board.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'board',
+    component: BoardComponent
+  },
+  {
+    path: '',
+    redirectTo:'board',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
